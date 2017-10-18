8464468a46cc5b025483b09f76e495c311626d50
alexis.grohar <alexis.grohar@rakuten.com>
2017-10-18 23:47:15 +0900

    moving to gradle

--- src/test/java/com/github/letsrokk/issues/CucumberJvmTest.java
+++
@@ -1,9 +0,0 @@
-package com.github.letsrokk.issues;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
-
-@CucumberOptions(features = {"src/test/resources/features/"})
-public class CucumberJvmTest extends AbstractTestNGCucumberTests {
-
-}
